89cdf72e87185152f3d4463cb1e63beb41107ff4
Anthony Labaere <anthony.labaere@gmail.com>
2018-08-29 14:30:31 +0200

    Externalisation de la lecture des enregistrements

--- src/app/_services/sound.service.ts
+++ src/app/_services/sound.service.ts
@@ -1,15 +1,19 @@
 import {Injectable} from '@angular/core';
-import { SoundData } from '../entities';
+import { SoundData, Sound } from '../entities';
 import { HttpClient } from '@angular/common/http';
+import { NativeAudio } from '@ionic-native/native-audio';
 
 @Injectable()
 export class SoundService {
 
     private static SOUNDS_DATA_FILE_PATH = '../assets/data/sounds.json';
 
+    private static AUDIO_FILES_PATH = 'assets/sounds/';
+    private static AUDIO_FILE_EXTENSION = '.mp3';
+
     private soundsData: SoundData[];
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private nativeAudio: NativeAudio) {}
 
     public getSoundsData(): Promise<SoundData[]> {
         if (this.soundsData) {
@@ -27,5 +31,20 @@ export class SoundService {
                 );
             });
         }
+    }
+
+    public playSound(sound: Sound) {
+        // TODO : jouer les mp3 d'une manière plus éléguante ?
+        this.nativeAudio.preloadSimple(sound.fileName, SoundService.AUDIO_FILES_PATH + sound.fileName + SoundService.AUDIO_FILE_EXTENSION)
+            .then((res: any) => {})
+            .catch(error => {
+                console.log('Impossible de charger le fichier ' + sound.fileName + SoundService.AUDIO_FILE_EXTENSION)
+            });
+  
+        this.nativeAudio.play(sound.fileName)
+            .then((res: any) => {})
+            .catch(error => {
+                console.log('Impossible de jouer le fichier ' + sound.fileName + SoundService.AUDIO_FILE_EXTENSION)
+            });
     }
 }
